38c41062b2150a676ebaed01d5e1a989d8164b37
Debanjan Poddar <debanjan2602@gmail.com>
2023-06-21 19:33:41 +0530

    Fixed a css bug

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -15,7 +15,7 @@ const DesktopSidebar = ({ currentUser }: DesktopSidebarProps) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:flex lg:flex-col justify-between">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
             <nav className="mt-4 flex flex-col justify-between">
                 <ul
                     role="list"
