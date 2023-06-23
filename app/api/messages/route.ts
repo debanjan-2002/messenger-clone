5c6ff224042dbed8443c523cf54f472d9d5e99df
Debanjan Poddar <debanjan2602@gmail.com>
2023-06-24 00:22:13 +0530

    Added pusher server logic to update add message and update conversation

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,6 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
-
+import { pusherServer } from "@/app/libs/pusher";
 import prisma from "@/app/libs/prismadb";
 
 export async function POST(request: Request) {
@@ -59,6 +59,20 @@ export async function POST(request: Request) {
                     }
                 }
             }
+        });
+
+        await pusherServer.trigger(conversationId, "messages:new", newMessage);
+
+        const lastMessage =
+            updatedConversation.messages[
+                updatedConversation.messages.length - 1
+            ];
+
+        updatedConversation.users.map(user => {
+            pusherServer.trigger(user.email!, "conversation:update", {
+                id: conversationId,
+                messages: [lastMessage]
+            });
         });
 
         return NextResponse.json(newMessage);
